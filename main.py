2b4087daa6c892742d960b9095f234392eb0b8c7
AluAsano <128302356+AluAsano@users.noreply.github.com>
2023-04-29 19:03:45 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,22 +1,18 @@
-a = 100
-tkb = 5.6
-skb = 5.9
-vtb = 4.28
-sber = 4.0
-sum_str = input("Укажите сумму, которую хотите внести : ")
-sum = int(sum_str)
-tkb_dp = sum / a * tkb
-skb_dp = sum / a * skb
-vtb_dp = sum / a * vtb
-sber_dp = sum / a * sber
-tkb_dp = int(tkb_dp)
-skb_dp = int(skb_dp)
-vtb_dp = int(vtb_dp)
-sber_dp = int(sber_dp)
-print("Депозит в банке 'ТКБ' составит: ", tkb_dp)
-print("Депозит в банке 'СКБ' составит: ", skb_dp)
-print("Депозит в банке 'ВТБ' составит: ", vtb_dp)
-print("Депозит в банке 'СБЕР' составит: ", sber_dp)
-all_dp = [tkb_dp, skb_dp, vtb_dp, sber_dp]
-max_dp = max(all_dp)
-print("Наибольший депозит составит: ", max_dp)
+total_tickets = int(input('Укажите какое кол-во билетов вы хотите зарезервировать : '))
+ticket = 1
+midl_ticket = 0
+elder_ticket = 0
+while ticket <= total_tickets:
+    ticket_x = int(input('Укажите возраст владельца билета : '))
+    if ticket_x > 0:
+        if 18 <= ticket_x < 25:
+            midl_ticket += 1
+        elif ticket_x >= +25:
+            elder_ticket += 1
+        ticket += 1
+    else:
+        print('Возраст указан неверно!')
+sum_ = midl_ticket * 990 + elder_ticket * 1390
+if total_tickets > 3:
+    sum_ = sum_ / 100 * 90
+print('Общая стоимость билетов : ', int(sum_))
